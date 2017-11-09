cb164a4141b0ae96fe34a06266a2b84ceb2fc9b8
Jesse <jessehwang86@yahoo.com>
2017-11-09 13:03:24 -0500

    task #3 - Objects, objects, objects!

---
+++ task3/objects_irb_shells.rb
@@ -0,0 +1,38 @@
+#################################
+#### Terminal #1 IRB Session ####
+#################################
+class Apple
+
+  attr_reader :variety, :origin, :history
+  def initialize(**args)
+    @variety = args[:variety]
+    @origin = args[:origin]
+    @history = args[:history]
+  end
+end
+
+apple = Apple.new(variety: 'Honeycrisp', origin: 'Minnesota', history: 'Introduction to Market: 1991')
+
+require 'YAML'
+
+File.open('apple.rb', 'w') {|f| f.write(YAML.dump(apple)) }
+# => 101
+
+
+#################################
+#### Terminal #2 IRB Session ####
+#################################
+class Apple
+
+  attr_reader :variety, :origin, :history
+  def initialize(**args)
+    @variety = args[:variety]
+    @origin = args[:origin]
+    @history = args[:history]
+  end
+end
+
+require 'YAML'
+
+apple = YAML.load(File.read('apple.rb'))
+
